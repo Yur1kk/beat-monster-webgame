763aff707c5ce0e3a00aa1cb8a159771c17bd703
Yurii Soroka <urasor04@gmail.com>
2024-07-30 13:23:36 +0300

    Added try-catch functionality to handle errors in userInput

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -11,13 +11,25 @@ const LOG_EVENT_MONSTER_ATTACK = "MONSTER_ATTACK";
 const LOG_EVENT_PLAYER_HEAL = "PLAYER_HEAL";
 const LOG_EVENT_GAME_OVER = "GAME_OVER";
 
-const enteredValue = prompt("Maximum life for you and the monster.", "100");
 
-let chosenMaxLife = parseInt(enteredValue);
 let battleLog = [];
 
-if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
+const getMaxLifeValues = () => {
+  const enteredValue = prompt('Maximum life for you and the monster.', '100');
+  
+  const parsedValue = parseInt(enteredValue);
+  if (isNaN(parsedValue) || parsedValue <= 0) {
+    throw {message: 'Invalid user inpit, not a number!'}; 
+  }
+  return parsedValue;
+}
+let chosenMaxLife;
+try {
+  chosenMaxLife = getMaxLifeValues ();
+} catch (error) {
+  console.log(error);
   chosenMaxLife = 100;
+  alert('You entered something wrong, default value wa set to 100!');
 }
 
 let currentMonsterHealth = chosenMaxLife;
